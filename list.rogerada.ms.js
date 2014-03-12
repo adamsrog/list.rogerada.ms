2f8ca0024da0d819760fc61fc6b4d2f10de57f19
Roger Adams <adamsrog04@gmail.com>
2014-03-11 22:59:45 -0700

    through the second video

--- list.rogerada.ms.js
+++ list.rogerada.ms.js
@@ -1,19 +1,70 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to list.rogerada.ms.";
-  };
+Todos = new Meteor.Collection('todos');
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+if (Meteor.isClient) {
+  Template.TodosPanel.helpers({
+    items: function() {
+      return Todos.find({}, {
+        sort: {
+          created_at: -1
+        }
+      });
+    },
+
+    isDoneClass: function() {
+      return this.is_done ? 'done' : '';
+    }
+  });
+
+  Template.TodoItem.helpers({
+    isDoneChecked: function() {
+      return this.is_done ? 'checked' : '';
+    }
+  });
+
+  Template.TodoItem.events({
+    'click [name=is_done]': function(e, tmpl) {
+      var id = this._id;
+      var isDone = tmpl.find('input').checked;
+      Todos.update({_id: id}, {
+        $set: {
+          is_done: isDone
+        }
+      });
+    }
+  });
+
+  Template.CreateTodoItem.events({
+    'submit form': function(e, tmpl) {
+      // prevent the default action from occuring (post to server)
+      e.preventDefault();
+
+      // get user's text
+      var subject = tmpl.find('input').value;
+      
+      // add to database
+      Todos.insert({
+        subject: subject,
+        created_at: new Date,
+        is_done: false
+      });
+
+      // clear the textbox
+      var form = tmpl.find('form');
+      form.reset();
+    }
+  });
+
+  Template.TodosCount.helpers({
+    completedCount: function () {
+      return Todos.find({is_done: true}).count();
+    },
+
+    totalCount: function() {
+      return Todos.find({}).count();
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
